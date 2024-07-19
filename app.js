a14e6cb07c38c2ae39eaa1e5a48c265eb0a833a9
MohamedEmadMaghawry <146999123+MohamedEmadMaghawry@users.noreply.github.com>
2024-07-19 21:52:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const forecast=require("./data1/forecast")
+const geocode=require("./data1/geocode")
+const country=process.argv[2]
+
+
+geocode(country,(error,data)=>{
+    console.log("error:",error)
+    console.log("data:",data)
+    forecast(data.latitude,data.longtitude,(error,data)=>{
+    console.log("error:",error)
+    console.log("data: ",data)
+})
+})
+
+
+// ///////////////////////////////////////////
+
